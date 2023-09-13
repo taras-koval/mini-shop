faa7d10d3a48f9c99188e849b6f76959bc8e70bb
Taras <tkoval37@gmail.com>
2023-09-13 18:56:02 +0200

    Add cast for order status

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\OrderStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -17,6 +18,10 @@ class Order extends Model
         'customer_phone',
         'comment',
         'status',
+    ];
+
+    protected $casts = [
+        'status' => OrderStatus::class
     ];
 
     public function product(): BelongsTo
