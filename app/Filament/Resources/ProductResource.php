12fac4e4bf5016a0c3f870220147023bc223dace
taras-koval <tkoval37@gmail.com>
2023-11-07 14:59:31 +0100

    Update Product Resource

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -6,8 +6,11 @@ use App\Filament\Resources\ProductResource\Pages;
 use App\Models\Product;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Resources\Pages\ListRecords\Tab;
 use Filament\Resources\Resource;
+use Filament\Support\Enums\FontWeight;
 use Filament\Tables;
+use Filament\Tables\Actions\Action;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -22,56 +25,53 @@ class ProductResource extends Resource
 
     public static function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('slug')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\Textarea::make('description')
-                    ->maxLength(65535)
-                    ->columnSpanFull(),
-                Forms\Components\FileUpload::make('image')
-                    ->image(),
-                Forms\Components\Toggle::make('is_active')
-                    ->required(),
-                Forms\Components\TextInput::make('price')
-                    ->required()
-                    ->numeric()
-                    ->prefix('$'),
-            ]);
+        return $form->schema([
+            Forms\Components\TextInput::make('name')->required()->maxLength(255),
+            Forms\Components\TextInput::make('slug')->required()->maxLength(255),
+            Forms\Components\Textarea::make('description')->maxLength(65535)->columnSpanFull(),
+            Forms\Components\FileUpload::make('image')->image(),
+            Forms\Components\Toggle::make('is_active')->required(),
+            Forms\Components\TextInput::make('price')->required()->numeric()->prefix('$'),
+        ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('name')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('slug')
-                    ->searchable(),
+                Tables\Columns\TextColumn::make('name')->searchable()->sortable()->weight(FontWeight::Bold),
                 Tables\Columns\ImageColumn::make('image'),
-                Tables\Columns\IconColumn::make('is_active')
-                    ->boolean(),
-                Tables\Columns\TextColumn::make('price')
-                    ->money()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime('d-m-Y H:i')
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime('d-m-Y H:i')
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\IconColumn::make('is_active')->boolean()->sortable(),
+                Tables\Columns\TextColumn::make('price')->money()->sortable(),
+
+                Tables\Columns\TextColumn::make('created_at')->dateTime('d-m-Y H:i')->sortable()->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')->dateTime('d-m-Y H:i')->sortable()->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')->dateTime('d-m-Y H:i')->sortable()->toggleable(isToggledHiddenByDefault: true),
             ])
+
+            ->defaultSort('created_at', 'desc')
             ->filters([
+                Tables\Filters\Filter::make('Is active')
+                    ->query(fn (Builder $query): Builder => $query->where('is_active', true)),
+                Tables\Filters\Filter::make('Is not active')
+                    ->query(fn (Builder $query): Builder => $query->where('is_active', false)),
                 Tables\Filters\TrashedFilter::make(),
             ])
             ->actions([
+                Action::make('Activate')
+                    ->action(function (Product $record) {
+                        $record->is_active = true;
+                        $record->save();
+                    })
+                    ->hidden(fn (Product $record): bool => $record->is_active),
+                Action::make('Deactivate')
+                    ->action(function (Product $record) {
+                        $record->is_active = false;
+                        $record->save();
+                    })
+                    ->visible(fn (Product $record): bool => $record->is_active),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -82,7 +82,18 @@ class ProductResource extends Resource
             ])
             ->emptyStateActions([
                 Tables\Actions\CreateAction::make(),
-            ]);
+            ])
+            ->paginated([10, 25, 50, 100])
+            ->defaultPaginationPageOption(25);
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'active' => Tab::make()->modifyQueryUsing(fn (Builder $query) => $query->where('is_active', true)),
+            'inactive' => Tab::make()->modifyQueryUsing(fn (Builder $query) => $query->where('is_active', false)),
+        ];
     }
 
     public static function getRelations(): array
