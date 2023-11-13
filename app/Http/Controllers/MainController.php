d963bcfec13b87da6a525667a01c3e866ff28202
taras-koval <tkoval37@gmail.com>
2023-11-13 03:29:45 +0100

    Implement filter

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $products = Product::where('is_active', true)
-            ->paginate(9);
+            ->filter($request)
+            ->paginate(9)
+            ->appends($request->all());
 
         return view('home', compact('products'));
     }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function show(Product $product)
     {
         return view('product.show', compact('product'));
+    }
+
+    public function testFilters(Request $request)
+    {
+        $products = Product::filter($request)->get();
+
+        return response()->json($products);
     }
 }
